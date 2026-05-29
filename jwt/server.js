582c88b84f6cf3f7c78b1d07d1e024d6c26da60b
Saurabh Yadav <sy5605781@gmail.com>
2026-05-29 15:01:57 +0530

    jwt authentication system

---
+++ jwt/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const express = require("express")
+
+const connectDb = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+connectDb()
+
+app.use(express.json())
+
+app.use("/api/auth", authRoutes)
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`)
+})
